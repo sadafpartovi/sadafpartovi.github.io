371b06b4f3b4f033a77a50564869a8b66bece79c
sadafpartovi <74632037+sadafpartovi@users.noreply.github.com>
2021-01-28 13:42:39 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const chevronsParent = document.querySelector("#scroll");
+const portfolio = document.querySelector("#portfolio");
+
+
+
+
+
+
+
+
+chevronsParent.addEventListener('click', () => {
+    portfolio.scrollIntoView();
+  });
+
